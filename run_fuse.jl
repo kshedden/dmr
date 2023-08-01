13b45c59456c919150af2819b90d4816d9b3612b
kshedden <kshedden@umich.edu>
2023-08-01 09:39:38 -0400

    Improve comments

--- run_fuse.jl
+++ run_fuse.jl
@@ -6,34 +6,40 @@ using Statistics
 using Printf
 using CSV
 
-pa = "/nfs/turbo/lsa-bis/data_for_Kerby/illumina"
+# Write the results here
 dp = "/nfs/turbo/lsa-bis/data_for_Kerby/illumina_fuse"
-fn = "Sample_XXXX_CpG.bedGraph"
+
+# Run the algorithm on these samples
+sample_ids = ["5554", "5555", "5663"]
 
 include("utils.jl")
 
 invlogit(x) = 1 / (1 + exp(-x))
 
 function fit_model(meth, total)
-    bb = fit(BetaBinomSeq, meth, total; maxiter=10, fuse_mean_wt=1.0, fuse_icc_wt=1.0,
+    bb = fit(BetaBinomSeq, meth, total; maxiter=10, fuse_mean_wt=1.5, fuse_icc_wt=1.5,
              step0=0.01, rho0=0.5, rhofac=1.1, rhomax=2.0, verbose=true)
     return bb
 end
 
 function fitstats(bb)
+    # Lengths of consecutive runs of constant means
     rr = diff(findall(diff(bb.logit_mean) .!= 0))
     qmean = median(rr)
     rmean = mean(rr)
+
+    # Lengths of consecutive runs of constant iccs
     rr = diff(findall(diff(bb.logit_icc) .!= 0))
     qicc = median(rr)
     ricc = mean(rr)
-    return (median_mean_gap=qmean, mean_mean_gap=rmean, median_icc_gap=qicc, mean_icc_gap=ricc)
+
+    return (median_mean_gap=qmean, mean_mean_gap=rmean,
+            median_icc_gap=qicc, mean_icc_gap=ricc)
 end
 
-function main()
-    chrom = "1"
-    out = open(dp, "$(chrom).log", "w")
-    for id in ["5554", "5555", "5663"]
+function run_chromosome(chrom)
+    out = open(joinpath(dp, "$(chrom).log"), "w")
+    for id in sample_ids
         da = prep_data(id, chrom)
         bb = fit_model(da[:, :meth], da[:, :total])
         st = fitstats(bb)
@@ -52,4 +58,4 @@ function main()
     close(out)
 end
 
-main()
+run_chromosome("1")
